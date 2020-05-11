2ce1d9e28f1baceb2883b1919a3e3b5a3af0ade6
unknown <1115159016@qq.com>
2020-05-11 11:28:15 +0800

    can modify plan

--- app/src/main/java/group3/sse/bupt/note/PlanActivity.java
+++ app/src/main/java/group3/sse/bupt/note/PlanActivity.java
@@ -1,18 +1,25 @@
 package group3.sse.bupt.note;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlanActivity extends AppCompatActivity {
+
+public class PlanActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private PlanDatabase dbHepler;
 
@@ -29,6 +36,7 @@ public class PlanActivity extends AppCompatActivity {
         adapter=new PlanAdapter(getApplicationContext(),planList);
         refreshView();
         lv.setAdapter(adapter);
+        lv.setOnItemClickListener(this);
         new_plan=findViewById(R.id.new_plan);
         new_plan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,10 +55,23 @@ public class PlanActivity extends AppCompatActivity {
         String content=data.getExtras().getString("content",null);
         String time=data.getExtras().getString("time",null);
         Plan newPlan=new Plan(content,time);
-        Log.i("hcccc","newplan"+newPlan.getTime());
+     //   Log.i("hcccc","newplan"+newPlan.getTime());
+        newPlan(newPlan);
+
+    }
+
+    public void newPlan(Plan newPlan){
         DBConnector dbConnector=new DBConnector(context);
         dbConnector.open();
         dbConnector.addPlan(newPlan);
+        dbConnector.close();
+        refreshView();
+    }
+
+    public void modifyPlan(Plan plan){
+        DBConnector dbConnector=new DBConnector(context);
+        dbConnector.open();
+        dbConnector.updatePlan(plan);
         dbConnector.close();
         refreshView();
     }
@@ -64,5 +85,32 @@ public class PlanActivity extends AppCompatActivity {
 
         dbConnector.close();
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+      switch (parent.getId()){
+          case R.id.lv:
+              Log.i("hcccc","onclicked");
+              final Plan curPlan=(Plan) parent.getItemAtPosition(position);
+              AlertDialog.Builder modifyDialog=new AlertDialog.Builder(PlanActivity.this);
+              final EditText editText=new EditText(PlanActivity.this);
+              editText.setText(curPlan.getContent());
+              modifyDialog.setTitle("修改plan").setView(editText);
+
+              modifyDialog.setPositiveButton("完成",
+                      new DialogInterface.OnClickListener() {
+                          @Override
+                          public void onClick(DialogInterface dialog, int which) {
+                              Plan modifiedplan=new Plan(editText.getText().toString(),curPlan.getTime());
+                              modifiedplan.setId(curPlan.getId());
+
+                              modifyPlan(modifiedplan);
+                          }
+                      }
+              ).show();
+              break;
+      }
     }
 }
